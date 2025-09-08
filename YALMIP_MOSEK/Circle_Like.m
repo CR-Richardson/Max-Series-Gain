ede9696f20a7b24eb7e3db34400e1e8376ba1432
Carl Richardson <80678360+CR-Richardson@users.noreply.github.com>
2025-09-08 10:29:03 +0100

    Update Circle_Like.m

--- YALMIP_MOSEK/Circle_Like.m
+++ YALMIP_MOSEK/Circle_Like.m
@@ -32,6 +32,11 @@ C     = syst.c;
 D     = syst.d;
 [n,m] = size(B); % n = dimension of state, m = dimension of output
 
+% Raise error if D != 0.
+if ~isequal(D, zeros(m))
+    error('D terms must be added to LMIs.', m, m);
+end
+
 %% Initialising alpha
 alpha = alpha_up;
 
